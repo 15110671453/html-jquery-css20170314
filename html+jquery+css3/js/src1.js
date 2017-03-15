fac939a1f4a845743fe6b432ab545eacb3c6d003
15110671453 <15110671453@163.com>
2017-03-15 15:50:17 +0800

    query bootstrap html 第一个页面
    query bootstrap html 第一个页面

---
+++ html+jquery+css3/js/src1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by admindyn on 2017/3/15.
+ */
+window.onload=function(){
+    document.getElementById('id-div-body').innerHTML+='<br><h3>body-script</h3>';
+
+
+}
---
+++ html+jquery+css3/js/src2.js
@@ -0,0 +1,4 @@
+/**
+ * Created by admindyn on 2017/3/15.
+ */
+alert('我要阻塞页面执行 阻塞页面顺利显示');
